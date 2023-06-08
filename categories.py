2ff18730779c0fdac91503ddd01b1235172ca9d0
Aldin Smajkan <39434215+Smajkan@users.noreply.github.com>
2023-06-08 15:40:01 +0200

    Update categories.py
    Made it faster, added exporting to JSON file

--- categories.py
+++ categories.py
@@ -1,7 +1,14 @@
 import requests
+import concurrent.futures
+import json
 
 # Replace {token} with your actual token
-token = "{token}" #It is not required by default to obtain category ids
+token = "{token}"  # It is not required by default to obtain category ids
+
+# Arrays to store categories
+main_cats = []
+children_cats = []
+children_of_children_cats = []
 
 # Function to retrieve and print categories
 def print_categories():
@@ -11,7 +18,7 @@ def print_categories():
     }
 
     # Retrieve all categories
-    response = requests.get("https://api.olx.ba/categories", headers=headers)
+    response = session.get("https://api.olx.ba/categories", headers=headers)
     data = response.json()
 
     if "data" in data:
@@ -20,6 +27,11 @@ def print_categories():
         # Print main categories and their IDs
         print("Main Categories:")
         for category in categories:
+            category_data = {
+                "id": category["id"],
+                "name": category["name"]
+            }
+            main_cats.append(category_data)
             print(f"ID: {category['id']}, Name: {category['name']}")
 
             # Retrieve and print children categories
@@ -29,7 +41,7 @@ def print_categories():
 # Function to retrieve and print children categories
 def print_children_categories(category_id, headers):
     # Retrieve children categories
-    response = requests.get(f"https://api.olx.ba/categories/{category_id}", headers=headers)
+    response = session.get(f"https://api.olx.ba/categories/{category_id}", headers=headers)
     data = response.json()
 
     if "data" in data and isinstance(data["data"], list):
@@ -38,15 +50,25 @@ def print_children_categories(category_id, headers):
         # Print children categories and their IDs
         print("Children Categories:")
         for category in children_categories:
+            category_data = {
+                "id": category["id"],
+                "name": category["name"]
+            }
+            children_cats.append(category_data)
             print(f"ID: {category['id']}, Name: {category['name']}")
             print_children_of_children_categories(category['id'], headers)
 
 def print_children_of_children_categories(category_id, headers):
-    response = requests.get(f"https://api.olx.ba/categories/{category_id}", headers=headers)
+    response = session.get(f"https://api.olx.ba/categories/{category_id}", headers=headers)
     data = response.json()
 
     if "data" in data and isinstance(data["data"], dict):
         category_info = data["data"]
+        category_data = {
+            "id": category_info["id"],
+            "name": category_info["name"]
+        }
+        children_of_children_cats.append(category_data)
         print("Children of children Categories:")
         print(f"ID: {category_info['id']}, Name: {category_info['name']}")
         # You can further process sub_categories if needed
@@ -55,5 +77,20 @@ def print_children_of_children_categories(category_id, headers):
         #     print(f"ID: {sub_category['id']}, Name: {sub_category['name']}")
         #     print_children_of_children_categories(sub_category['id'], headers)
 
+# Create a requests session object
+session = requests.Session()
+
 # Print categories
 print_categories()
+
+# Export main_cats to JSON file
+with open("main_cats.json", "w") as file:
+    json.dump(main_cats, file, indent=4)
+
+# Export children_cats to JSON file
+with open("children_cats.json", "w") as file:
+    json.dump(children_cats, file, indent=4)
+
+# Export children_of_children_cats to JSON file
+with open("children_of_children_cats.json", "w") as file:
+    json.dump(children_of_children_cats, file, indent=4)
